9c86cc549ae337111a30782fac950da72f8ff890
s0larw1nd <alixess13@gmail.com>
2025-01-23 14:25:24 +0300

    Added home page, changed routes

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,7 +2,22 @@ class Author < ApplicationRecord
   has_many :genre
   has_one_attached :photo
 
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 15 }, format: { with: /\A[А-Яа-яЁё]+\z/ }
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 15 }, format: { with: /\A[А-Яа-яЁё]+\z/ }
+  validates :patronymics,  presence: true, length: { minimum: 2, maximum: 15 }, format: { with: /\A[А-Яа-яЁё]+\z/ }
+  validates :nationality, length: { minimum: 4, maximum: 15 }, format: { with: /\A[А-Яа-яЁё]+\z/ }
+
+  validate :check_dates
+
   def display_name
     "#{first_name} #{last_name} #{patronymics}"
+  end
+
+  private
+
+  def check_dates
+    if birthdate.present? && deathdate.present? && birthdate > deathdate
+      errors.add(:deathdate, "Некорректные даты рождения и смерти")
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index', as: 'home'
+
   resources :books
   resources :authors
   resources :genres
